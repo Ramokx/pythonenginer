4089feb4ac9bebeaab28b618343bc4bc0a63f8a8
Ramokx <bagrov86@gmail.com>
2024-09-08 20:45:50 +0300

    7_3b исправил, не работал со словарем(не учитывался факт неправильного ввода)

--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -17,14 +17,18 @@ Enter VLAN number: 10
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
-vlan_in = int(input('Enter VLAN number: '))
-result = {}
+vlan_in = input('Enter VLAN number: ')
+#result = {}
 with open('CAM_table.txt') as file:
     for line in file:
         data = line.split()
-        if data and data[0].isdigit():
+        if data and data[0].isdigit() and data[0] == vlan_in:
             vlan, mac, ports = int(data[0]), data[1], data[-1]
-            result.setdefault(vlan, []).append([vlan, mac, ports])
+            print(f'{vlan:<10}{mac:20}{ports:8}')
 
-for item in result[vlan_in]:
-    print('{:<10}{:20}{:8}'.format(*item))
+
+            #это для словаря
+            #result.setdefault(vlan, []).append([vlan, mac, ports])
+
+#for item in result[vlan_in]:
+   # print('{:<10}{:20}{:8}'.format(*item))
